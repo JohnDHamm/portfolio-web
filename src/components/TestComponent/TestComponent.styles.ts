c2da29c0cb94a5c409ad6b246d2fd40285954e51
John Hamm <johndhamm1@gmail.com>
2020-09-29 15:06:46 -0500

    setup: CRA, typescript, styled-components, storybook

---
+++ src/components/TestComponent/TestComponent.styles.ts
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  border: 1px solid #bada55;
+  padding: 0 4rem;
+  border-radius: 10px;
+`;
---
+++ src/components/TestComponent/TestComponent.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Container } from './TestComponent.styles';
+const TestComponent: React.FC = () => {
+  return (
+    <Container>
+      <h2>Test Component</h2>
+    </Container>
+  );
+};
+
+export default TestComponent;
