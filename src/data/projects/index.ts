0d28bcb15d665bdeb37503b942b6bb8951c512e2
John Hamm <johndhamm1@gmail.com>
2020-10-23 10:20:50 -0500

    feat(data): start projects data

---
+++ src/data/projects/index.ts
@@ -0,0 +1,74 @@
+const TeamTimerPro_Project: Project = {
+  id: 'teamtimerpro',
+  title: 'Team Timer Pro',
+  colors: {
+    primary: 'navy',
+  },
+  description: [
+    `We're coming! Come on! Come on! Oh, Artoo, hurry! My goodness! Artoo, why did you have to be so brave? Well, I suppose I could hotwire this thing. I'll cover you.`,
+    `Of course I'm worried. And you should be, too. Lando Calrissian and poor Chewbacca never returned from this awful place. Artoo whistles timidly. Don't be so sure. If I told you half the things I've heard about this Jabba the Hutt, you'd probably short-circuit. Artoo, are you sure this is the right place? I better knock, I suppose. There doesn't seem to be anyone there. Let's go back and tell Master Luke.`,
+  ],
+  cardImage: require('../../mocks/projects/images/mockproject1.png'),
+};
+
+const DidDoDone_Project: Project = {
+  id: 'diddodone',
+  title: 'Did? Do! Done.',
+  colors: {
+    primary: '#4c408f',
+  },
+  description: [
+    `We're coming! Come on! Come on! Oh, Artoo, hurry! My goodness! Artoo, why did you have to be so brave? Well, I suppose I could hotwire this thing. I'll cover you.`,
+    `Of course I'm worried. And you should be, too. Lando Calrissian and poor Chewbacca never returned from this awful place. Artoo whistles timidly. Don't be so sure. If I told you half the things I've heard about this Jabba the Hutt, you'd probably short-circuit. Artoo, are you sure this is the right place? I better knock, I suppose. There doesn't seem to be anyone there. Let's go back and tell Master Luke.`,
+  ],
+  cardImage: require('./images/diddodone_card.png'),
+  screencaps: require('./images/diddodone_screencaps.png'),
+  techIcons: require('./images/diddodone_tech.png'),
+};
+
+const CadenceCoach_Project: Project = {
+  id: 'cadencecoach',
+  title: 'Cadence Coach',
+  colors: {
+    primary: 'black',
+  },
+  description: [
+    `We're coming! Come on! Come on! Oh, Artoo, hurry! My goodness! Artoo, why did you have to be so brave? Well, I suppose I could hotwire this thing. I'll cover you.`,
+    `Of course I'm worried. And you should be, too. Lando Calrissian and poor Chewbacca never returned from this awful place. Artoo whistles timidly. Don't be so sure. If I told you half the things I've heard about this Jabba the Hutt, you'd probably short-circuit. Artoo, are you sure this is the right place? I better knock, I suppose. There doesn't seem to be anyone there. Let's go back and tell Master Luke.`,
+  ],
+  // cardImage: require('../../mocks/projects/images/mockproject1.png'),
+};
+
+const Nog_Project: Project = {
+  id: 'nog',
+  title: 'Nog',
+  colors: {
+    primary: 'red',
+  },
+  description: [
+    `We're coming! Come on! Come on! Oh, Artoo, hurry! My goodness! Artoo, why did you have to be so brave? Well, I suppose I could hotwire this thing. I'll cover you.`,
+    `Of course I'm worried. And you should be, too. Lando Calrissian and poor Chewbacca never returned from this awful place. Artoo whistles timidly. Don't be so sure. If I told you half the things I've heard about this Jabba the Hutt, you'd probably short-circuit. Artoo, are you sure this is the right place? I better knock, I suppose. There doesn't seem to be anyone there. Let's go back and tell Master Luke.`,
+  ],
+  // cardImage: require('../../mocks/projects/images/mockproject1.png'),
+};
+
+const TheBigBoard_Project: Project = {
+  id: 'thebigboard',
+  title: 'The Big Board',
+  colors: {
+    primary: '#bada55',
+  },
+  description: [
+    `We're coming! Come on! Come on! Oh, Artoo, hurry! My goodness! Artoo, why did you have to be so brave? Well, I suppose I could hotwire this thing. I'll cover you.`,
+    `Of course I'm worried. And you should be, too. Lando Calrissian and poor Chewbacca never returned from this awful place. Artoo whistles timidly. Don't be so sure. If I told you half the things I've heard about this Jabba the Hutt, you'd probably short-circuit. Artoo, are you sure this is the right place? I better knock, I suppose. There doesn't seem to be anyone there. Let's go back and tell Master Luke.`,
+  ],
+  // cardImage: require('../../mocks/projects/images/mockproject1.png'),
+};
+
+export const projectsData: Project[] = [
+  TeamTimerPro_Project,
+  DidDoDone_Project,
+  Nog_Project,
+  CadenceCoach_Project,
+  TheBigBoard_Project,
+];
