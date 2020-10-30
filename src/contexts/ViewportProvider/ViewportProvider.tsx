b0de9bc051c2a8db7e7595fcaecd47ab484862e5
John Hamm <johndhamm1@gmail.com>
2020-10-30 11:10:08 -0500

    feat(contexts): add ViewportProvider

---
+++ src/contexts/ViewportProvider/ViewportProvider.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+export const viewportContext = React.createContext({ width: 0, height: 0 });
+
+export const ViewportProvider: React.FC = ({ children }) => {
+  const [width, setWidth] = React.useState<number>(window.innerWidth);
+  const [height, setHeight] = React.useState<number>(window.innerHeight);
+
+  const handleWindowResize = () => {
+    setWidth(window.innerWidth);
+    setHeight(window.innerHeight);
+  };
+
+  React.useEffect(() => {
+    window.addEventListener('resize', handleWindowResize);
+    return () => window.removeEventListener('resize', handleWindowResize);
+  }, []);
+
+  return (
+    <viewportContext.Provider value={{ width, height }}>
+      {children}
+    </viewportContext.Provider>
+  );
+};
