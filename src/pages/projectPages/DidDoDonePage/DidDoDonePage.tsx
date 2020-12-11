c2bbfcd3eb89952e33c6c6486e2b2f8a2abc306c
John Hamm <johndhamm1@gmail.com>
2020-12-11 13:29:07 -0600

    feat(project pages): use template, add project images

--- src/pages/projectPages/DidDoDonePage/DidDoDonePage.tsx
+++ src/pages/projectPages/DidDoDonePage/DidDoDonePage.tsx
@@ -1,22 +1,12 @@
 import React from 'react';
 import {
-  AnimatedPageTransition,
   AppStoreButton,
-  Button,
   DesktopSlideshow,
-  GooglePlayButton,
   MobileSlideshow,
+  ProjectPageTemplate,
   TechSectionPresenter,
-  VideoPlayer,
 } from '../../../components';
-import {
-  CenteredContent,
-  ContentWithMargins,
-  HomeLink,
-  Logo,
-  StyledPageWrapper,
-  StyledTextBlock,
-} from '../styles';
+import { ContentWithMargins } from '../styles';
 import { DidDoDone_Project } from '../../../data/projects';
 import DDDlogo from '../../../data/projects/diddodone/diddodone_logo.png';
 import { TECH_ICONS } from '../../../assets/images/tech_icons';
@@ -26,85 +16,31 @@ import {
   DidDoDoneMobileScreenshots,
 } from '../../../data/projects/diddodone/mobile_slideshow';
 
-const DESKTOP_TEST01 = require('../../../mocks/desktop_screenshots_slideshow_test01.png');
-const DESKTOP_TEST02 = require('../../../mocks/desktop_screenshots_slideshow_test02.png');
-const DESKTOP_TEST03 = require('../../../mocks/desktop_screenshots_slideshow_test03.png');
-
 const { colors } = DidDoDone_Project;
 
 export const DidDoDonePage: React.FC = () => {
   return (
-    <StyledPageWrapper bgColor={colors.primary}>
-      <AnimatedPageTransition
-        isVisible={true}
-        type="end"
-        color={colors.primary}
-      />
-      <CenteredContent>
-        <div style={{ width: '100%' }}>
-          <HomeLink href="/">&lt; home</HomeLink>
-        </div>
-        <Logo src={DDDlogo} alt="logo" />
-        <StyledTextBlock>
-          <p>Did Do Done</p>
-        </StyledTextBlock>
-        <StyledTextBlock>
-          <p>
-            Of course I'm worried. And you should be, too. Lando Calrissian and
-            poor Chewbacca never returned from this awful place. Artoo whistles
-            timidly. Don't be so sure. If I told you half the things I've heard
-            about this Jabba the Hutt, you'd probably short-circuit. Artoo, are
-            you sure this is the right place? I better knock, I suppose. There
-            doesn't seem to be anyone there. Let's go back and tell Master Luke.
-          </p>
-        </StyledTextBlock>
-        <ContentWithMargins top="2rem" bottom="4rem">
-          <DesktopSlideshow
-            images={[DESKTOP_TEST01, DESKTOP_TEST02, DESKTOP_TEST03]}
-            captions={[
-              'This is the caption for image 1.',
-              'Another caption.',
-              "This is the caption for image 3. Let's make it a longer caption to test how it looks wrapping.",
-            ]}
-          />
-        </ContentWithMargins>
-        <ContentWithMargins top="2rem" bottom="4rem">
-          <MobileSlideshow
-            images={DidDoDoneMobileScreenshots}
-            captions={DidDoDoneMobileCaptions}
-          />
-        </ContentWithMargins>
-        <ContentWithMargins top="2rem" bottom="4rem">
-          <VideoPlayer
-            url="https://player.vimeo.com/video/200288091?color=ffffff&title=0&byline=0&portrait=0"
-            caption="Here's some caption for the video."
-          />
-        </ContentWithMargins>
-        <TechSectionPresenter
-          color={COLORS.WHITE}
-          techIcons={[
-            TECH_ICONS.REACT_NATIVE,
-            TECH_ICONS.LOTTIE,
-            TECH_ICONS.ADOBE_XD,
-          ]}
-          githubURL="https://github.com/JohnDHamm/did-do-done-RN-app"
+    <ProjectPageTemplate
+      projectColor={colors.primary}
+      logo={{ src: DDDlogo, alt: 'Did Do Done logo', width: 380 }}
+      mainText="This is where the main text goes about the project"
+    >
+      <ContentWithMargins top="2rem" bottom="4rem">
+        <MobileSlideshow
+          images={DidDoDoneMobileScreenshots}
+          captions={DidDoDoneMobileCaptions}
         />
-        <AppStoreButton url="https://apps.apple.com/us/app/did-do-done/id1522794964" />
-        <GooglePlayButton url="https://play.google.com/store/apps/details?id=com.cadencecoach" />
-        <ContentWithMargins top="1rem" bottom="1rem">
-          <a
-            href="http://demo.nogworld.com"
-            target="_blank"
-            rel="noopener noreferrer"
-            style={{ textDecoration: 'none' }}
-          >
-            <Button>TRY THE DEMO</Button>
-          </a>
-        </ContentWithMargins>
-        <ContentWithMargins top="3rem" bottom="0rem">
-          <HomeLink href="/">&lt; home</HomeLink>
-        </ContentWithMargins>
-      </CenteredContent>
-    </StyledPageWrapper>
+      </ContentWithMargins>
+      <TechSectionPresenter
+        color={COLORS.WHITE}
+        techIcons={[
+          TECH_ICONS.REACT_NATIVE,
+          TECH_ICONS.LOTTIE,
+          TECH_ICONS.ADOBE_XD,
+        ]}
+        githubURL="https://github.com/JohnDHamm/did-do-done-RN-app"
+      />
+      <AppStoreButton url="https://apps.apple.com/us/app/did-do-done/id1522794964" />
+    </ProjectPageTemplate>
   );
 };
--- src/pages/projectPages/NogPage/NogPage.tsx
+++ src/pages/projectPages/NogPage/NogPage.tsx
@@ -1,18 +1,75 @@
 import React from 'react';
-import { AnimatedPageTransition } from '../../../components';
+import {
+  Button,
+  DesktopSlideshow,
+  MobileSlideshow,
+  ProjectPageTemplate,
+  TechSectionPresenter,
+  VideoPlayer,
+} from '../../../components';
+import { ContentWithMargins } from '../styles';
+import NogLogo from '../../../data/projects/nog/nog_logo.png';
+import { TECH_ICONS } from '../../../assets/images/tech_icons';
+import { COLORS } from '../../../styles';
+import {
+  DidDoDoneMobileCaptions,
+  DidDoDoneMobileScreenshots,
+} from '../../../data/projects/diddodone/mobile_slideshow';
 import { Nog_Project } from '../../../data/projects';
+const DESKTOP_TEST01 = require('../../../mocks/desktop_screenshots_slideshow_test01.png');
+const DESKTOP_TEST02 = require('../../../mocks/desktop_screenshots_slideshow_test02.png');
+const DESKTOP_TEST03 = require('../../../mocks/desktop_screenshots_slideshow_test03.png');
 
 const { colors } = Nog_Project;
 
 export const NogPage: React.FC = () => {
   return (
-    <div style={{ position: 'relative' }}>
-      <AnimatedPageTransition
-        isVisible={true}
-        type="end"
-        color={colors.primary}
+    <ProjectPageTemplate
+      projectColor={colors.primary}
+      logo={{ src: NogLogo, alt: 'Nog logo', width: 440 }}
+      mainText="This is where the main text goes about the project"
+    >
+      <ContentWithMargins top="2rem" bottom="4rem">
+        <DesktopSlideshow
+          images={[DESKTOP_TEST01, DESKTOP_TEST02, DESKTOP_TEST03]}
+          captions={[
+            'This is the caption for image 1.',
+            'Another caption.',
+            "This is the caption for image 3. Let's make it a longer caption to test how it looks wrapping.",
+          ]}
+        />
+      </ContentWithMargins>
+      <ContentWithMargins top="2rem" bottom="4rem">
+        <MobileSlideshow
+          images={DidDoDoneMobileScreenshots}
+          captions={DidDoDoneMobileCaptions}
+        />
+      </ContentWithMargins>
+      <ContentWithMargins top="2rem" bottom="4rem">
+        <VideoPlayer
+          url="https://player.vimeo.com/video/200288091?color=ffffff&title=0&byline=0&portrait=0"
+          caption="Here's some caption for the video."
+        />
+      </ContentWithMargins>
+      <TechSectionPresenter
+        color={COLORS.WHITE}
+        techIcons={[
+          TECH_ICONS.REACT_NATIVE,
+          TECH_ICONS.LOTTIE,
+          TECH_ICONS.ADOBE_XD,
+        ]}
+        githubURL="https://github.com/JohnDHamm/nog-react"
       />
-      <div>Nog</div>
-    </div>
+      <ContentWithMargins top="1rem" bottom="1rem">
+        <a
+          href="http://demo.nogworld.com"
+          target="_blank"
+          rel="noopener noreferrer"
+          style={{ textDecoration: 'none' }}
+        >
+          <Button>TRY THE DEMO</Button>
+        </a>
+      </ContentWithMargins>
+    </ProjectPageTemplate>
   );
 };
