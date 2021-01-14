0e60e0f92b98e56a26aa2b6f70e39a41f040e003
John Hamm <johndhamm1@gmail.com>
2021-01-14 13:40:35 -0600

    feat(assets, HomePage): add tech icons, tech section of About

--- src/assets/images/tech_icons/index.ts
+++ src/assets/images/tech_icons/index.ts
@@ -1,4 +1,16 @@
 export const TECH_ICONS: TechIcons = {
+  ADOBE_AE: {
+    URL: require('./adobe-ae.png'),
+    ALT: 'adobe after effects',
+  },
+  ADOBE_AI: {
+    URL: require('./adobe-ai.png'),
+    ALT: 'adobe illustrator',
+  },
+  ADOBE_PS: {
+    URL: require('./adobe-ps.png'),
+    ALT: 'adobe photoshop',
+  },
   ADOBE_XD: {
     URL: require('./adobe-xd.png'),
     ALT: 'adobe xd',
@@ -7,6 +19,14 @@ export const TECH_ICONS: TechIcons = {
     URL: require('./arduino.png'),
     ALT: 'arduino',
   },
+  AWS_S3: {
+    URL: require('./aws_s3.png'),
+    ALT: 'aws_s3',
+  },
+  CYPRESS: {
+    URL: require('./cypress.png'),
+    ALT: 'cypress',
+  },
   EXPO: {
     URL: require('./expo.png'),
     ALT: 'expo',
@@ -14,6 +34,26 @@ export const TECH_ICONS: TechIcons = {
   EXPRESS: {
     URL: require('./express.png'),
     ALT: 'express',
+  },
+  GIT: {
+    URL: require('./git.png'),
+    ALT: 'git',
+  },
+  HEROKU: {
+    URL: require('./heroku.png'),
+    ALT: 'heroku',
+  },
+  JEST: {
+    URL: require('./jest.png'),
+    ALT: 'jest',
+  },
+  JIRA: {
+    URL: require('./jira.png'),
+    ALT: 'jira',
+  },
+  JS_ES6: {
+    URL: require('./js_es6.png'),
+    ALT: 'javascript ES6',
   },
   LOTTIE: {
     URL: require('./lottie_logo.png'),
@@ -35,6 +75,10 @@ export const TECH_ICONS: TechIcons = {
     URL: require('./node-express.png'),
     ALT: 'node js + express',
   },
+  POSTGRES: {
+    URL: require('./postgres.png'),
+    ALT: 'postgres',
+  },
   REACT: {
     URL: require('./react.png'),
     ALT: 'react',
@@ -50,5 +94,13 @@ export const TECH_ICONS: TechIcons = {
   SOCKET_IO: {
     URL: require('./socket_io.png'),
     ALT: 'socket io',
+  },
+  STYLED_COMPONENTS: {
+    URL: require('./styled-components.png'),
+    ALT: 'styled-components',
+  },
+  TYPESCRIPT: {
+    URL: require('./typescript.png'),
+    ALT: 'typescript',
   },
 };
